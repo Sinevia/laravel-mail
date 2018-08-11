e703f76ae71d9a41012752bffe6642f70868ad2b
Sinevia <info@sinevia.com>
2018-08-11 04:20:39 +0100

    Create MailServiceProvider.php

---
+++ src/MailServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sinevia\Mail;
+
+use Illuminate\Support\ServiceProvider;
+
+class SineviaMailServiceProvider extends \Illuminate\Mail\MailServiceProvider {
+
+    protected function registerSwiftTransport() {
+//        if ($this->app['config']['mail.driver'] == 'sinevia_mail') {
+//            $this->app['swift.mailer'] = $this->app->share(function ($app) {
+//                return new \Swift_Mailer(new SineviaMailTransport());
+//            });
+
+            $this->app->singleton('swift.transport', function ($app) {
+                return new MailTransportManager($app);
+            });
+//        } else {
+//            parent::registerSwiftMailer();
+//        }
+    }
+
+}
